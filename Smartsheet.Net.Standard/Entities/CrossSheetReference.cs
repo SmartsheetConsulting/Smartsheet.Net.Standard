607ba422cc7ece25d9dc64dc5904b4ae1cb872b0
mishaberman <mishaberman@gmail.com>
2018-07-27 15:17:21 -0700

    list cross sheet refs

---
+++ Smartsheet.Net.Standard/Entities/CrossSheetReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Smartsheet.Net.Standard.Interfaces;
+
+namespace Smartsheet.Net.Standard.Entities {
+    public class CrossSheetReference : ISmartsheetObject {
+        public long Id { get; set; }
+        public long EndColumnId { get; set; }
+        public long EndRowId { get; set; }
+        public long SourceSheetId { get; set; }
+        public long StartColumnId { get; set; }
+        public long StartRowId { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
