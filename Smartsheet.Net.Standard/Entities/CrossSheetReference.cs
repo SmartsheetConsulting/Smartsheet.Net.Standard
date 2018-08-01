9deb62cfab7d350e838408c0439dbdf31f27a125
mishaberman <mishaberman@gmail.com>
2018-08-01 13:35:30 -0700

    made everything nullable long for cross sheet ref object

--- Smartsheet.Net.Standard/Entities/CrossSheetReference.cs
+++ Smartsheet.Net.Standard/Entities/CrossSheetReference.cs
@@ -3,11 +3,11 @@
 namespace Smartsheet.Net.Standard.Entities {
     public class CrossSheetReference : ISmartsheetObject {
         public long? Id { get; set; }
-        public long EndColumnId { get; set; }
-        public long EndRowId { get; set; }
-        public long SourceSheetId { get; set; }
-        public long StartColumnId { get; set; }
-        public long StartRowId { get; set; }
+        public long? EndColumnId { get; set; }
+        public long? EndRowId { get; set; }
+        public long? SourceSheetId { get; set; }
+        public long? StartColumnId { get; set; }
+        public long? StartRowId { get; set; }
         public string Name { get; set; }
         public string Status { get; set; }
     }
